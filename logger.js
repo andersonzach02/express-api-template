fb37a8198575362590702a8e7a5f46ac6ce2ad6e
andersonzach02 <andersonzach02@gmail.com>
2021-11-25 14:51:00 -0500

    Fixed an error with one of the winston logging transports which was incorrectly using the 'errors' logging level rather than the 'error' logging level. Now have made it so that errors get logged to the error.log file when an error occurs.

--- logger.js
+++ logger.js
@@ -1,29 +1,29 @@
 const winston = require('winston');
 
 const winstonLogger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
-  transports: [
-    new winston.transports.File({
-      filename: './logs/error.log',
-      level: 'errors',
-    }),
-    new winston.transports.File({ filename: './logs/combined.log' }),
-  ],
-  stream: {
-    /* eslint-disable object-shorthand, func-names */
-    write: (message) => winstonLogger.http(message),
-    /* eslint-enable object-shorthand, func-names */
-  },
+	level: 'info',
+	format: winston.format.json(),
+	transports: [
+		new winston.transports.File({
+			filename: './logs/error.log',
+			level: 'error',
+		}),
+		new winston.transports.File({ filename: './logs/combined.log' }),
+	],
+	stream: {
+		/* eslint-disable object-shorthand, func-names */
+		write: (message) => winstonLogger.http(message),
+		/* eslint-enable object-shorthand, func-names */
+	},
 });
 
 if (process.env.NODE_ENV !== 'production') {
-  winstonLogger.add(
-    new winston.transports.Console({
-      format: winston.format.simple(),
-      colorize: true,
-    }),
-  );
+	winstonLogger.add(
+		new winston.transports.Console({
+			format: winston.format.simple(),
+			colorize: true,
+		})
+	);
 }
 
 module.exports = winstonLogger;
